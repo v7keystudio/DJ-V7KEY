1e79a2bb74d1294787b7b3604814c231d9996c10
Anurag S Yadav <anurag6200633@gmail.com>
2026-08-26 15:43:00 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+const firebaseConfig = {
+    apiKey: "YOUR_NEW_API_KEY_HERE",
+    authDomain: "alone-5c341.firebaseapp.com",
+    databaseURL: "https://alone-5c341-default-rtdb.firebaseio.com",
+    projectId: "alone-5c341",
+    storageBucket: "alone-5c341.firebasestorage.app",
+    messagingSenderId: "429965309146",
+    appId: "1:429965309146:web:b462a3f9082dfb624e61e3",
+    measurementId: "G-2J549ZQ4NN"
+};
